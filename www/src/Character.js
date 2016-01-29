9786ab84cd012a45290afd05cdb1c62e5739791c
joshjb84@hotmail.com <joshjb84@hotmail.com>
2016-01-30 10:20:51 +1100

    some refactoring and making him go faster

--- www/src/Character.js
+++ www/src/Character.js
@@ -2,6 +2,7 @@ var Character = (function(){
     function Character(game, x, y, sprite){
         Phaser.Sprite.call(this, game, x, y, sprite);
 
+        this.speed = 5;
         this.game = game;
 
         this.game.physics.arcade.enable(this);
@@ -18,6 +19,23 @@ var Character = (function(){
 
     Character.prototype = Object.create(Phaser.Sprite.prototype);
     Character.prototype.constructor = Character;
+
+    Character.prototype.moveUp = function(){
+        this.y -= this.speed;
+        this.animations.play('up');
+    };
+    Character.prototype.moveDown = function(){
+        this.y += this.speed;
+        this.animations.play('down');
+    };
+    Character.prototype.moveLeft = function(){
+        this.x -= this.speed;
+        this.animations.play('left');
+    };
+    Character.prototype.moveRight = function(){
+        this.x += this.speed;
+        this.animations.play('right');
+    };
 
     return Character;
 })();
