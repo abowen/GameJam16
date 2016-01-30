1a13ad154385217b7b04fc0bf5dfcd135d981a77
abowen <andrew.james.bowen@gmail.com>
2016-01-30 19:52:08 +1100

    Added array helped function + screenshake setup

--- www/src/World.js
+++ www/src/World.js
@@ -24,10 +24,18 @@ var World = (function() {
             	counter = [5,10,15]
             }
 
-        	this.world.setBounds(-20, -20, this.game.width+20, this.game.height+2);
+            var max = this.screenShake.counter.max();
+        	this.world.setBounds(-max, -max, this.game.width+max, this.game.height+2);
         };
         
         World.prototype.constructor = World;
+
+        World.prototype.screenShake: function(effect) {        	
+	        var min = -effect;
+	        var max = effect;
+	        this.game.camera.x += Math.floor(Math.random() * (max - min + 1)) + min;
+	        this.game.camera.y += Math.floor(Math.random() * (max - min + 1)) + min;
+    	};
         
         World.prototype.devourHuman = function (){
             this.player.humans_devoured += 1;
