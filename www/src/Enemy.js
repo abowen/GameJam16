735ca8962cffd20644ae9b866641df1695e4b939
abowen <andrew.james.bowen@gmail.com>
2016-01-30 23:21:10 +1100

    Eating on the spot

--- www/src/Enemy.js
+++ www/src/Enemy.js
@@ -48,87 +48,96 @@ var Enemy = (function() {
 
     Enemy.prototype.update = function() {
         "use strict";
-        var new_position;
-        //this.game.physics.arcade.collide(this, this.game.groups.humans, this.switch_direction, null, this);
-        // this.game.physics.arcade.collide(this, this.game_state.layers.blocks, this.switch_direction, null, this);
-        // this.game.physics.arcade.overlap(this, this.game_state.groups.bombs, this.switch_direction, null, this);
-        // this.game.physics.arcade.overlap(this, this.game_state.groups.explosions, this.kill, null, this);
-        var followed_mob = {};
-        var dist = 0.0;
-        if (this.game_state.humans && this.game_state.humans.children.length > 0) {
 
-            var closest_human = {};
-            var dist_min = 99999;
-            this.game_state.humans.forEachAlive(function(human) {
-                var current_dist = this.game.math.distance(this.body.position.x,
+        if (this.game_state.world_state.enemy.isEatingHuman){
+            this.gotTheGiggles();
+        }
+        else {
+
+            var new_position;
+            //this.game.physics.arcade.collide(this, this.game.groups.humans, this.switch_direction, null, this);
+            // this.game.physics.arcade.collide(this, this.game_state.layers.blocks, this.switch_direction, null, this);
+            // this.game.physics.arcade.overlap(this, this.game_state.groups.bombs, this.switch_direction, null, this);
+            // this.game.physics.arcade.overlap(this, this.game_state.groups.explosions, this.kill, null, this);
+            var followed_mob = {};
+            var dist = 0.0;
+            if (this.game_state.humans && this.game_state.humans.children.length > 0) {
+
+                var closest_human = {};
+                var dist_min = 99999;
+                this.game_state.humans.forEachAlive(function(human) {
+                    var current_dist = this.game.math.distance(this.body.position.x,
+                        this.body.position.y,
+                        human.position.x,
+                        human.position.y);
+                    
+                    if(dist < dist_min){
+                        dist = dist_min;
+                        current_dist = dist_min;
+                        closest_human = human;
+                    }
+                }, this);
+
+                followed_mob = closest_human;
+            } else {
+                followed_mob = this.game_state.character;
+                dist = this.game.math.distance(this.body.position.x,
                     this.body.position.y,
-                    human.position.x,
-                    human.position.y);
-                
-                if(dist < dist_min){
-                    dist = dist_min;
-                    current_dist = dist_min;
-                    closest_human = human;
-                }
-            }, this);
+                    followed_mob.position.x,
+                    followed_mob.position.y);
+            }
 
-            followed_mob = closest_human;
-        } else {
-            followed_mob = this.game_state.character;
-            dist = this.game.math.distance(this.body.position.x,
-                this.body.position.y,
-                followed_mob.position.x,
-                followed_mob.position.y);
-        }
+            var vel_factor = 1.0;
 
-        var vel_factor = 1.0;
+            if (dist > 20.0) {
+                this.game.ai.follow(this, followed_mob, this.base_speed * vel_factor, this.base_speed * vel_factor);
+            } else {
+                this.stop();
+            }
 
-        if (dist > 20.0) {
-            this.game.ai.follow(this, followed_mob, this.base_speed * vel_factor, this.base_speed * vel_factor);
-        } else {
-            this.stop();
-        }
+            if (this.body.velocity.x < 0) {
+                // walking left
+                this.scale.setTo(-1, 1);
+                this.animations.play("left");
+            } else if (this.body.velocity.x > 0) {
+                // walking right
+                this.scale.setTo(1, 1);
+                this.animations.play("right");
+            }
 
-        if (this.body.velocity.x < 0) {
-            // walking left
-            this.scale.setTo(-1, 1);
-            this.animations.play("left");
-        } else if (this.body.velocity.x > 0) {
-            // walking right
-            this.scale.setTo(1, 1);
-            this.animations.play("right");
-        }
-
-        if (this.body.velocity.y < 0) {
-            // walking up
-            this.animations.play("up");
-        } else if (this.body.velocity.y > 0) {
-            // walking down
-            this.animations.play("down");
-        }
+            if (this.body.velocity.y < 0) {
+                // walking up
+                this.animations.play("up");
+            } else if (this.body.velocity.y > 0) {
+                // walking down
+                this.animations.play("down");
+            }        
+        }    
 
         if (this.body.velocity.x === 0 && this.body.velocity.y === 0) {
-            // stop current animation
-            this.animations.stop();
-            //this.frame = this.stopped_frames[this.body.facing];
-        }
+                // stop current animation
+                this.animations.stop();
+                //this.frame = this.stopped_frames[this.body.facing];
+            }
 
         new_position = (this.axis === "x") ? this.x : this.y;
         if (Math.abs(new_position - this.previous_position) >= this.walking_distance) {
-            this.switch_direction();
+                this.switch_direction();
         }
-
-        this.gotTheGiggles();
     };
 
-    Enemy.prototype.gotTheGiggles = function() {
-        if (this.game_state.world_state.enemy.isEatingHuman){
-            var max = 2;
-            var min = 1;
-            //var movement = Math.floor(Math.random() * (max - min + 1)) + min;
-            //console.log('Got the giggles');
-            //this.body.velocity.x
-        }
+    Enemy.prototype.gotTheGiggles = function() {        
+        this.body.velocity.x = 0;
+        this.body.velocity.y = 0;
+        var max = 2;
+        var min = 1;
+        // TODO: Make him bigger
+
+        // TODO: Make him wobble
+
+        //var movement = Math.floor(Math.random() * (max - min + 1)) + min;
+        //console.log('Got the giggles');
+        //this.body.velocity.x        
     };
 
     Enemy.prototype.switch_direction = function() {
