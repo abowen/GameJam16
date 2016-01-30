5d95f1591cc4baf36b41639fda990a65b3a4ce16
joshjb84@hotmail.com <joshjb84@hotmail.com>
2016-01-30 20:49:55 +1100

    smarter humans

--- www/src/Game.js
+++ www/src/Game.js
@@ -250,7 +250,7 @@ BasicGame.Game.prototype = {
 
     update: function() {
         this.physics.arcade.collide(this.backgroundLayer, this.characters);
-        this.physics.arcade.collide(this.backgroundLayer, this.humans);
+
         this.physics.arcade.collide(this.backgroundLayer, this.enemy);
 
         /*  if (this.game.game_state.player.rituals_performed === this.game.game_state.win_conditions.rituals_performed) {
