a9431fdd3171a6468b6a050ff04e25a07396ac67
abowen <andrew.james.bowen@gmail.com>
2016-01-30 14:50:00 +1100

    Changed back to NO_SCALE

--- www/src/Game.js
+++ www/src/Game.js
@@ -29,7 +29,7 @@ BasicGame.Game.prototype = {
         // * SHOW_ALL
         // * RESIZE
         // See http://docs.phaser.io/Phaser.ScaleManager.html for full document
-        this.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
+        this.scale.scaleMode = Phaser.ScaleManager.NO_SCALE;
         this.scale.setUserScale(2, 2, 0, 0);
         // If you wish to align your game in the middle of the page then you can
         // set this value to true. It will place a re-calculated margin-left
