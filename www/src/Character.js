50cf027add5c09ec7eebc28383b2f0f75fc42b3e
dider <dider7@gmail.com>
2016-01-30 18:02:39 +1100

    Human screams while getting killed.

--- www/src/Character.js
+++ www/src/Character.js
@@ -42,6 +42,9 @@ var Character = (function(){
         this.x += this.speed;
         this.animations.play('right');
     };
+    Character.prototype.kill = function() {
+        this.destroy();
+    }
 
     return Character;
 })();
