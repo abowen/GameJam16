6dd7f159287a24cabea45a4110e5242fcfac293b
Dominik Raniszewski <dominik.raniszewski@outlook.com>
2016-01-30 22:46:38 +1100

    animated offering stone

--- www/src/OfferingStone.js
+++ www/src/OfferingStone.js
@@ -12,11 +12,19 @@ var OfferingStone = (function() {
             y: y
         }, properties);
         
+        this.framePerSecond = 7;
         this.anchor.setTo(0.5);
         game_state.game.physics.arcade.enable(this);
         this.body.immovable = true;
         this.setAnimations();
         this.animations.play("wait");
+        
+        this.performRitual = function(character, offeringStone) {            
+            this.animations.play("ritual");            
+             setTimeout(function() {
+                this.animations.play("wait");
+            }.bind(this), 2000);
+        }
     };
     
     OfferingStone.prototype = Object.create(Phaser.Sprite.prototype);
@@ -24,7 +32,11 @@ var OfferingStone = (function() {
     
     OfferingStone.prototype.setAnimations = function(){
         this.animations.add('wait', [0, 1, 2, 3], this.framesPerSecond, true);
-       // this.animations.add('ritual', [4, 5, 6, 7], this.framesPerSecond, true);
+        this.animations.add('ritual', [4, 5, 6, 7], this.framesPerSecond, true);
+    };
+    
+    OfferingStone.prototype.update = function(){
+        this.game_state.game.physics.arcade.collide(this, this.game_state.character, this.performRitual, null, this);
     };
     
     return OfferingStone;
