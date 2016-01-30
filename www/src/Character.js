342525a6e216c8fe0c0497dcd74f04b254ad3d64
dider <dider7@gmail.com>
2016-01-30 12:45:46 +1100

    Used different spritesheet for Human

--- www/src/Character.js
+++ www/src/Character.js
@@ -10,16 +10,21 @@ var Character = (function(){
         this.anchor.set(0.5);
 
         // animation name, frames, FPS, true? (maybe swap)
-        var framesPerSecond = 10;
-        this.animations.add('down', [0, 1, 2], framesPerSecond, true);
-        this.animations.add('right', [3, 4, 5], framesPerSecond, true);
-        this.animations.add('up', [6, 7, 8], framesPerSecond, true);
-        this.animations.add('left', [9, 10, 11], framesPerSecond, true);
+        this.framesPerSecond = 10;        
         this.body.collideWorldBounds = true;
+
+        this.setAnimation();
     };
 
     Character.prototype = Object.create(Phaser.Sprite.prototype);
     Character.prototype.constructor = Character;
+
+    Character.prototype.setAnimation = function() {
+        this.animations.add('down', [0, 1, 2], this.framesPerSecond, true);
+        this.animations.add('right', [3, 4, 5], this.framesPerSecond, true);
+        this.animations.add('up', [6, 7, 8], this.framesPerSecond, true);
+        this.animations.add('left', [9, 10, 11], this.framesPerSecond, true);
+    };
 
     Character.prototype.moveUp = function(){
         this.y -= this.speed;
--- www/src/Human.js
+++ www/src/Human.js
@@ -3,10 +3,21 @@ var Human = (function() {
 		Character.call(this, game, x, y, sprite);
 		this.speed = 1;
 		this.body.acceleration = {x: 0, y: this.game.rnd.between(0, 50)};
+
+		this.framesPerSecond = 15;
+        
+        this.setAnimation();
 	};
 
 	Human.prototype = Object.create(Character.prototype);
 	Human.prototype.constructor = Human;
+
+	Human.prototype.setAnimation = function() {
+		this.animations.add('down', [0, 1, 2, 3, 4, 5], this.framesPerSecond, true);
+        this.animations.add('right', [18, 19, 20, 21, 22, 23], this.framesPerSecond, true);
+        this.animations.add('up', [12, 13, 14, 15, 16, 17], this.framesPerSecond, true);
+        this.animations.add('left', [6, 7, 8, 9, 10, 11], this.framesPerSecond, true);
+	};
 
 	Human.prototype.update = function() {
 		this.moveDown();        
