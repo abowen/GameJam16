196a8ae493d232883ef9232bb05f4c24118a4de9
joshjb84@hotmail.com <joshjb84@hotmail.com>
2016-01-30 19:47:48 +1100

    adding collisions for character

--- www/src/MovingSprite.js
+++ www/src/MovingSprite.js
@@ -7,8 +7,8 @@ var MovingSprite = (function(){
 
         Prefab.call(this, game_state, 'Player', {x:x, y:y}, properties);
 
-        this.speed = 2;
         game = game_state.game;
+        this.speed = 80;
 
         game.physics.enable(this);
         this.anchor.set(0.5);
@@ -20,20 +20,24 @@ var MovingSprite = (function(){
     MovingSprite.prototype = Object.create(Phaser.Sprite.prototype);
     MovingSprite.prototype.constructor = MovingSprite;
 
+    MovingSprite.prototype.setVelocity = function(x, y){
+        this.body.velocity.x = x;
+        this.body.velocity.y = y;
+    },
     MovingSprite.prototype.moveUp = function(){
-        this.y -= this.speed;
+        this.setVelocity(0, -this.speed);
         this.animations.play('up');
     };
     MovingSprite.prototype.moveDown = function(){
-        this.y += this.speed;
+        this.setVelocity(0, this.speed);
         this.animations.play('down');
     };
     MovingSprite.prototype.moveLeft = function(){
-        this.x -= this.speed;
+        this.setVelocity(-this.speed, 0);
         this.animations.play('left');
     };
     MovingSprite.prototype.moveRight = function(){
-        this.x += this.speed;
+        this.setVelocity(this.speed, 0);
         this.animations.play('right');
     };
 
