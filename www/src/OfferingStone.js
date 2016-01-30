d7ed820f5ccb09cf8aca5b74afa0a51b25948a54
Dominik Raniszewski <dominik.raniszewski@outlook.com>
2016-01-30 22:34:36 +1100

    animated stone

--- www/src/OfferingStone.js
+++ www/src/OfferingStone.js
@@ -15,11 +15,17 @@ var OfferingStone = (function() {
         this.anchor.setTo(0.5);
         game_state.game.physics.arcade.enable(this);
         this.body.immovable = true;
-  
+        this.setAnimations();
+        this.animations.play("wait");
     };
     
     OfferingStone.prototype = Object.create(Phaser.Sprite.prototype);
     OfferingStone.prototype.constructor = OfferingStone;
+    
+    OfferingStone.prototype.setAnimations = function(){
+        this.animations.add('wait', [0, 1, 2, 3], this.framesPerSecond, true);
+       // this.animations.add('ritual', [4, 5, 6, 7], this.framesPerSecond, true);
+    };
     
     return OfferingStone;
 })();
