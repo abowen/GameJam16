294448387b5b19e99fafec6acfb363451ca769d0
abowen <andrew.james.bowen@gmail.com>
2016-01-30 18:14:49 +1100

    Ghosts a little faster

--- www/src/Ghost.js
+++ www/src/Ghost.js
@@ -2,7 +2,7 @@ var Ghost = (function() {
     function Ghost(game, x, y) {
         MovingSprite.call(this, game, x, y, 'ghost', 'ghosts');
 
-        this.speed = 0.5;
+        this.speed = 1.5;
         this.game = game;
         this.timer = 1000;
         this.framesPerSecond = 3;
@@ -56,7 +56,7 @@ var Ghost = (function() {
                     this.human.position.x,
                     this.human.position.y);
 
-                var vel_factor = 1.0;
+                var vel_factor = this.speed;
 
                 if (dist > 20.0) {
                     this.game.ai.follow(this, this.game.character, 30.0 * vel_factor, 30.0 * vel_factor);
