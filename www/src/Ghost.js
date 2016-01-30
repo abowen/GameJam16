e3db2cab6502072ea78663d2d579ec2b331a8fc9
Dominik Raniszewski <dominik.raniszewski@outlook.com>
2016-01-30 16:20:08 +1100

    Following ghosts

--- www/src/Ghost.js
+++ www/src/Ghost.js
@@ -15,9 +15,10 @@ var Ghost = (function() {
         this.setAnimation();
 
         this.moves = ['moveUp', 'moveDown', 'moveLeft', 'moveRight'];
+        this.game.physics.arcade.enable(this);
         this.lastMove = null;
         this.isGraveStone = true;
-
+        this.animations.play("grave");
         setTimeout(function() {
             this.isGraveStone = false;
         }.bind(this), 4000);
@@ -36,10 +37,12 @@ var Ghost = (function() {
     }
 
     Ghost.prototype.setAnimation = function() {
-        this.animations.add('down', [1, 2, 3], this.framesPerSecond, true);
-        this.animations.add('right', [4, 5, 6], this.framesPerSecond, true);
-        this.animations.add('up', [7, 8, 9], this.framesPerSecond, true);
-        this.animations.add('left', [4, 5, 6], this.framesPerSecond, true);
+        this.animations.add('down', [0, 1, 2], this.framesPerSecond, true);
+        this.animations.add('right', [3, 4, 5], this.framesPerSecond, true);
+        this.animations.add('up', [6, 7, 8], this.framesPerSecond, true);
+        this.animations.add('left', [3, 4, 5], this.framesPerSecond, true);
+        this.animations.add('grave', [9], this.framesPerSecond, true);
+        this.stopped_frames = [1, 4, 4, 0, 1, 9];
     };
 
     Ghost.prototype.update = function() {
@@ -66,6 +69,30 @@ var Ghost = (function() {
                 }
                 this[moveIn]();
                 this.lastMove = moveIn;
+            }
+
+            if (this.body.velocity.x < 0) {
+                // walking left
+                this.scale.setTo(-1, 1);
+                this.animations.play("left");
+            } else if (this.body.velocity.x > 0) {
+                // walking right
+                this.scale.setTo(1, 1);
+                this.animations.play("right");
+            }
+
+            if (this.body.velocity.y < 0) {
+                // walking up
+                this.animations.play("up");
+            } else if (this.body.velocity.y > 0) {
+                // walking down
+                this.animations.play("down");
+            }
+
+            if (this.body.velocity.x === 0 && this.body.velocity.y === 0) {
+                // stop current animation
+                this.animations.stop();
+                this.frame = this.stopped_frames[this.body.facing];
             }
         } else {
             this.stop();
