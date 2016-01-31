4efbb4dc3f7550153234ec8235edebbd3c1c4243
abowen <andrew.james.bowen@gmail.com>
2016-01-31 13:11:47 +1100

    Refactored music into soundGroup

--- www/src/World.js
+++ www/src/World.js
@@ -150,14 +150,14 @@ var World = (function() {
         this.makeWorldScarierOrCooler();
     };
 
-    World.prototype.reset = function() {        
-       
-        
+    World.prototype.reset = function() {            
         this.refreshGroup("humans");
         this.refreshGroup("ghosts");
         this.refreshGroup("enemies");
         this.refreshGroup("scoreLayer");
         this.refreshGroup("bodyParts");     
+
+        this.game_state.levelMusicSoundGroup.playNextSound(true);
 
         this.game_state.enemies.addChild(new Enemy(this.game_state, 'Enemy', this.game_state.world.centerX + (this.game_state.world.centerX / 2), this.game_state.world.centerY, 'enemy'));
 
